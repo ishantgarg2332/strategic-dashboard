bf34f26f3eb96352018ea494b0321fb01d1dc61f
ishantgarg2332 <garg.ishant.dev@gmail.com>
2022-09-05 06:12:57 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from 'react';
+import './App.css';
+
+const App: FunctionComponent = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link"
+          href="https://reactjs.org"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
